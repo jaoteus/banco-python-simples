d88bc21b752d237a6803cea5a636a705dadf4152
Mateus <mateusff2323@gmail.com>
2023-10-29 10:30:23 -0300

    adicionado autenticacao

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 '''
 initial commit
 '''
+import os
 
 class Aplication():
     def __init__(self) -> None:
@@ -20,14 +21,51 @@ class Aplication():
         self.email = 'john0011@gmail.com'
 
         ''' Loops '''
-        self.loop_login = True
+        self.loop_menu = True
         self.loop_principal =True
 
-    #     ''' Funções '''
-    #     self.menu()
-    
-    # def menu(self):
-    #     print('D')
+        ''' Flags '''
+        self.autenticado = None
+
+        ''' Funções '''
+        self.login()
+        self.menu()
+
+    def login(self):
+        while self.autenticado == None:
+            self.email_ou_cpf = str(input('Digite seu e-mail ou CPF: '))
+            self.senha_login = str(input('Digite sua senha: '))
+            if (self.email_ou_cpf == self.email or self.email_ou_cpf == self.CPF and self.senha_login == self.senha):
+                os.system('cls')
+                print("Login efetuado com sucesso!")
+                self.autenticado = True
+            elif (self.email_ou_cpf == '' or self.senha_login == ''):
+                os.system('cls')
+                print("Alguns dos campos ficaram em branco, tente novamente!")
+                continue
+            else:
+                os.system('cls')
+                print("E-mail, CPF ou senha incorretos, tente novamente!")
+                continue
+
+    def menu(self):
+        print('1 - Dados da conta')
+        print('2 - Depósito na conta corrente')
+        print('3 - Saque na conta corrente')
+        print('4 - Depósito na conta poupança')
+        print('5 - Resgate na conta poupança')
+        print('6 - Sair')
+        while self.loop_menu:
+            self.valor = str(input("Digite: "))
+            match self.valor:
+                case '1':
+                    # Dados da conta
+                    break
+                case '2':
+                    # Depósito conta corrente
+                    break
+                case '3':
+                    # 
 
 
 
