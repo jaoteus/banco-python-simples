b3f2f4dfe6ddf5eca0e4e0feacc6353b492b0bd0
Mateus <mateusff2323@gmail.com>
2023-10-29 09:52:24 -0300

    adicionado variáveis necessárias

--- Main.py
+++ Main.py
@@ -1,3 +1,34 @@
 '''
 initial commit
 '''
+
+class Aplication():
+    def __init__(self) -> None:
+        ''' Variáveis '''
+        self.saldo_cc = 0.0
+        self.saldo_cp = 0.0
+        self.valor = 0.0
+        self.nome_titular = 'John'
+        self.senha = 'john0011'
+        self.CPF = '000.000.000-11'
+        self.numero_conta_corrente = '1234-1'
+        self.numero_conta_poupanca = '1234-2'
+        self.agencia = '2332-9'
+        self.endereco = 'Rua Seilá, n°01, Recife, Pernambuco.'
+        self.numero = '+55 00 0000-1111'
+        self.opcao_menu = ''
+        self.email = 'john0011@gmail.com'
+
+        ''' Loops '''
+        self.loop_login = True
+        self.loop_principal =True
+
+    #     ''' Funções '''
+    #     self.menu()
+    
+    # def menu(self):
+    #     print('D')
+
+
+
+Aplication()
